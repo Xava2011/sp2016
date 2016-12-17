d15136b589e164318830af7db08b9473f31f0c39
Michał Chodun <Xava2011@gmail.com>
2016-12-17 10:55:11 +0100

    popr el3zad3

--- elementarz3/zad3.c
+++ elementarz3/zad3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-  for(int a = 1; a < 334; a++) {
-    for(int b = a; b < 501; b++) {
+  for(int a = 1; a < 1000; a++) {
+    //334 501
+    for(int b = a; b < 1000; b++) {
       for(int c = b; a+b+c <= 1000 ; c++) {
         if (a*a + b*b == c*c) {
           printf("%d\t%d\t%d\n", a, b, c);
--- rekurencja/zad2.c
+++ rekurencja/zad2.c
@@ -6,13 +6,13 @@ int main() {
   printf("wd(6, 2) = %d\n", wd(6, 2));
   printf("wd(6, 0) = %d\n", wd(6, 0));
   printf("wd(6, 6) = %d\n", wd(6, 6));
-  // prin()
+  printf("wd(5, 6) = %d/n", wd(5, 6));
 }
 
-int wd(int n, int k) {
+  int wd(int n, int k) {
     if (k == 0 || k == n)
         return 1;
       else
-      return wd(n - 1, k -1) + wd(n -1, k);
+        return wd(n - 1, k -1) + wd(n -1, k);
 
     }
